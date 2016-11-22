19dd5868be5662c6a84f3e3df888e695231fdd5f
K2346 <K2346@student.jamk.fi>
2016-11-22 23:44:15 +0200

    faq aloitettu

--- uusi-veispuuk-react-app/src/scene/Home/FAQ.jsx
+++ uusi-veispuuk-react-app/src/scene/Home/FAQ.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import firebase from 'firebase';
+import $ from 'jquery';
+
+
+import { Button ,Modal } from 'react-bootstrap';
+
 
 var FAQ = React.createClass({
     render: function() {
         return (
-            <p>faq sivu</p>
+            <div className="container">
+            <h1 className="container_heading" >FAQ</h1>
+                <div className="row">
+                            <p>ggg</p>
+                            <p>Tänne sitten se profiili ja ne asetukset</p>
+                </div>
+            </div>
         );
     }
 });
