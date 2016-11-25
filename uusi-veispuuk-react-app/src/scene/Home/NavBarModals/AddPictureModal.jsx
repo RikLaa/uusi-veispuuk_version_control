5a62bfd0c00c49fc0ba6483ae6fc99874721f708
RikLaa <H3410@student.jamk.fi>
2016-11-25 17:54:02 +0200

    addpicture lisätty

--- uusi-veispuuk-react-app/src/scene/Home/NavBarModals/AddPictureModal.jsx
+++ uusi-veispuuk-react-app/src/scene/Home/NavBarModals/AddPictureModal.jsx
@@ -1,18 +1,69 @@
 import React from 'react';
-import { Modal } from 'react-bootstrap';
+import { Button, FormGroup, Form, FormControl, Col, Checkbox, ControlLabel, InputGroup, DropdownButton, MenuItem,FieldGroup, Modal, poover,tooltip, HelpBlock   } from 'react-bootstrap';
 
 var AddPictureModal = React.createClass({
+    
+        //Handle add picture
+  addPicture: function(e){
+      console.log(e.target);
+      var reader = new FileReader();
+
+    reader.onload = function (e) {
+        // get loaded data and render thumbnail.
+        console.log(e.target.result);
+        document.getElementById("image_thumb").src = e.target.result;
+    };
+
+    // read the image file as a data URL.
+    reader.readAsDataURL(e.target.files[0]);
+  },
+    
     render: function() {
         return (
             <div>
                 <Modal show={this.props.showModal} onHide={this.props.onClick}>
                         <Modal.Header closeButton>
-                            <Modal.Title>Lisää kuva</Modal.Title>
-                            <br />
-        
-                            <hr />
-
+                            <Modal.Title><h3 className="h3_center">Lisää kuva</h3></Modal.Title>
                         </Modal.Header>
+                                    <Modal.Body>
+            
+        <Form horizontal>
+        <div className="Row"> 
+           <div className="col-sm-12 coll-sm-offset"> 
+        <FormGroup controlId="formHorizontal">
+           <img id="image_thumb"/>
+            <h5 className="h5_center">Esikatselu</h5>
+            <div className="col-md-10 col-md-offset-3">
+            <label className="btn btn-default btn-file">
+            <input id="add_picture" type="file" id="uploadimage" name="datafile" size="45" onChange= {this.addPicture}  ref='picture'/>
+            </label>
+            </div>
+            </FormGroup>
+            </div>
+            <FormGroup controlId="formHorizontal">
+            <div className="col-sm-2 coll-sm-offset">
+            <ControlLabel>Valitse # </ControlLabel>
+            </div>
+            <div className="col-sm-10">
+             <FormControl inline componentClass="select" placeholder="select">
+           
+            <option value="other">#Pasin koodit</option>
+            <option value="other">#Vain insinööri jutut</option>
+            <option value="other">#Kissa</option>
+            <option value="other">#Kalja</option>
+            </FormControl>
+            </div>
+        </FormGroup>
+        </div>
+  </Form>        
+                            
+                    </Modal.Body>
+                <Modal.Footer>
+            <div className="col-sm-3 col-md-offset-9">
+           <Button type="submit" bsStyle="success" onClick={this.props.onClick}>Julkaise</Button>
+             </div>
+                </Modal.Footer>
+            
                     </Modal>
             </div>
         );
