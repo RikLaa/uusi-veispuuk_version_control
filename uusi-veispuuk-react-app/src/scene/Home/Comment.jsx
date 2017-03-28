cf71c1313f9b07ee89890405a9a111d875ef765f
RikLaa <riku.laajala@gmail.com>
2017-03-28 14:02:40 +0300

    comments viewing, adding works

--- uusi-veispuuk-react-app/src/scene/Home/Comment.jsx
+++ uusi-veispuuk-react-app/src/scene/Home/Comment.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 var Comment = React.createClass({
     render: function () {
         return (
-            <div key={this.props.keyValue}>
+            <div>
                 <p>{this.props.content}</p>
                 <p>{this.props.userName}</p>
                 <small>{this.props.date} klo {this.props.time}</small>
